8fe493bf615c66b9cc0ba3afc3a8d6a7cd7a73c9
lamdv <lam.dv@live.com>
2019-03-18 12:24:33 +0100

    180319

--- ELM.R
+++ ELM.R
@@ -23,8 +23,8 @@ head(obj.bigSNP$fam)
 W <- FBM(obj.bigSNP$genotypes$ncol, width, backingfile = ("weight"))
 #W <- big_attach("weight.rds.bk")
 W$show()
-W [] <- rnorm(length(W))
-# head(W[])
+W [] <- rnorm(length(W),mean=0,sd = 1/sqrt(ncol(obj.bigSNP$genotypes)))
+head(W[])
 
 #apply_weight <- function(X, ind) {
 #  print(min(ind))
@@ -35,12 +35,34 @@ W [] <- rnorm(length(W))
 #                   a.combine =h1 <- big_prodMat(W, 'c', ind = rows_along(obj.bigSNP$genotypes),
 #                   block.size = 100e3, ncores = nb_cores())  
 
-
+# ELM Steps
 h1 <- as_FBM(big_prodMat(obj.bigSNP$genotypes,W))
-sums <- big_colstats(h1)$sum
+hist(h1[,1])
+h1
+h1.out <- matrix(unlist(big_apply(h1, a.FUN = function(X, ind){
+  1/(1+exp((-X[,ind])))
+})), ncol = 1000, byrow = TRUE)
 
-h1.out <- big_apply(h1, function(X, ind){
-  return(tan(sum(X[ ,ind]))) 
-}, ind = ncol(h1))
+h1.out <- matrix(unlist(h1.out), ncol = 1000, byrow = TRUE)
 
-tan(sum(h1[100, ]))
+h1.out <- as_FBM(h1.out)
+
+# Logistic Regression on the output of hidden layer
+test <- big_univLogReg(h1.out, y01.train = obj.bigSNP$fam$affection)
+
+#########
+# Validation step
+#########
+
+snp_readBed(bedfile = paste(data_test,"bed",sep="."), backingfile = data_test)
+obj.bigSNP <- snp_attach(paste(data_test,"rds",sep = "."))
+
+# ELM Steps
+h1 <- as_FBM(big_prodMat(obj.bigSNP$genotypes,W))
+hist(h1[,1])
+h1
+h1.out <- matrix(unlist(big_apply(h1, a.FUN = function(X, ind){
+  1/(1+exp((-X[,ind])))
+})), ncol = 1000, byrow = TRUE)
+
+h1.out <- as_FBM(h1.out)
